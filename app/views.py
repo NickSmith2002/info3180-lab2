54c4acf7ffa69b4a7dc004e4ba36274ad9e0d5cb
Nicholas Smith <nicholaswsmith2002@gmail.com>
2024-02-20 11:25:23 -0500

    Implemented profile page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import datetime
 
 ###
 # Routing for your application.
@@ -16,6 +16,11 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    """Render the website's about page."""
+    return render_template('profile.html', date = format_date_joined("2024-02-20"))
 
 
 ###
@@ -45,3 +50,8 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+#Regular function
+def format_date_joined(date):
+    Date = datetime.strptime(date, '%Y-%m-%d')
+    return Date.strftime("%b, %Y")
